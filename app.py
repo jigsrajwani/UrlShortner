96ab1b94f8349f59d51d28f15e1b3c28deb3b857
jigsrajwani <jigyasarajwani2@gmail.com>
2024-02-04 17:25:20 +0530

    updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-
 class ShortUrl(BaseModel):
     full: str
     clicks: int = 0
@@ -18,15 +17,16 @@ short_urls = {}
 @app.post("/shortUrls")
 async def create_short_url(full_url: str = Form(...)):
     short_url = shortuuid.uuid()[:8]
-    # short_url = ShortUrl(full=full_url)
     short_urls.update({"short_url": short_url})
     short_urls.update({"full_url": full_url})
     return {"short_url": short_url, "full_url": full_url}
 
 @app.get("/{short_url}")
 async def redirect_to_full_url(short_url: str):
-    print(short_urls["full_url"])
-    return RedirectResponse(short_urls["full_url"])
+    full_url = short_urls.get(short_url)
+    if not full_url:
+        raise HTTPException(status_code=404, detail="Short URL not found")
+    return RedirectResponse(full_url)
 
 # Serve the HTML file and static assets
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
